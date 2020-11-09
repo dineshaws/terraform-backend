ead96f3cb192283d1e9b95c6467c7a2698371712
Dinesh Prajapati <dprajapati.jpr@gmail.com>
2020-11-09 07:35:13 +0530

    Removed tags from ec2 to check sentinal policy check

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "dineshaws-terraform-remote-backend"
-#     key    = "demo.tfstate"
-#     region = "us-east-1"
-#     dynamodb_table = "terraform-s3-state-lock"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "dineshaws-terraform-remote-backend"
+    key    = "demo.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-s3-state-lock"
+  }
+}
